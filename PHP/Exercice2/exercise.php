97ac499d6e6e093fe404806fb707f4c0262982d8
daniel.huebner <airbuslux@gmail.com>
2019-02-25 12:25:45 +0100

    erxercise 2

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,16 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+
+
+$firstPart = substr(
+    $password,
+    0,
+    floor(strlen($password)/2) + (strlen($password)%2)
+);
+
+$lastpart = substr($password, ceil(strlen($password) /2));
+$saltedPassword = $firstPart . $salt . $lastpart;
+
+
+
